c1d46079ec441f3468eea4ec0e6e27995b3f4a39
Kyle Purser <kyle.purser@tamu.edu>
2017-03-21 19:16:11 -0500

    Created initial project and scenes

---
+++ HCI_Prototypes/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public GameManager gManager = null;
+
+	// Use this for initialization
+	void Start () {
+
+		if (gManager == null) {
+			gManager = this;
+		} 
+		else if (gManager != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+	}
+
+	public void changeScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
