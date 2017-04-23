06bc48308e11a627c6803d56fadfc56f6a6a3c01
ahles-nicole <ahles_nicole@tamu.edu>
2017-04-23 18:30:55 -0500

    Drinks Menu and Draggable Items
    Added Images, set up the Drinks menu, made an item draggable

---
+++ HCI_Prototypes/Assets/Scripts/Draggable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public void OnBeginDrag(PointerEventData eventData) {
+		Debug.Log("OnBeginDrag");
+        
+        //this.transform.SetParent(this.transform.parent.parent)
+	}
+	
+	public void OnDrag(PointerEventData eventData) {
+		Debug.Log("OnDrag");
+        
+        this.transform.position = eventData.position;
+	}
+    
+    public void OnEndDrag(PointerEventData eventData) {
+		Debug.Log("OnEndDrag");
+	}
+}
